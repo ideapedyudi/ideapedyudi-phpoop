9dd03cb75531ecccaa860d8bf9ab3755874ba86d
codeplacebo <codeplacebo@gmail.com>
2021-01-01 21:04:22 +0700

    class dan object

---
+++ cobe.php
@@ -0,0 +1,14 @@
+<?php
+
+class Coba
+{
+}
+
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
---
+++ produk.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// jualan produk
+// komik
+// game
+
+class Produk
+{
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
